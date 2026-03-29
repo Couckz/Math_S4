aaa43f1d78d21e0e0a6aaed02748bd15e3529a14
Bensemmane Camélia <bensemmanecamelia@gmail.com>
2026-03-29 17:08:54 +0200

    d

--- Math_Julie/TP1/test1.py
+++ Math_Julie/TP1/test1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
 n = 5 #Nombre de points d'interpolation considéré
 points_x = np.linspace(-1,1,n) #Creation d'un intervalle de points avec le nombre de points considéré
 points_y = [5*i**4 + i**2 + 7 for i in points_x] #Calcul des images de ces points par la fonction choisie
@@ -11,6 +10,8 @@ def li_Lagrange(points_x, points_y):
     Polynome_Interpolé = [] #Définition d'une variable qui contiendra les valeurs évalués en points_x[i] du polynome interpolé
     X = np.linspace(-1, 1, 300) #Définition d'un axe des abscisses 
     Y = [5*x**4 + x**2 + 7 for x in X] #Définition d'un axe des ordonnées
+    
+    #Implementation du calcul des li de lagrange
     for x in X: 
         P_évalué = 0 #Valeur du polynome interpolé évalué 
         for j in range(len(points_x)):
@@ -19,16 +20,18 @@ def li_Lagrange(points_x, points_y):
                 if i != j:
                     li *= (x - points_x[i]) / (points_x[j] - points_x[i])
             P_évalué += points_y[j] * li
-        Polynome_Interpolé.append(P_évalué)
+        
+        Polynome_Interpolé.append(P_évalué) #Ajout de P(x) évalué en x à la liste contenant les images du polynome interpolé
     
     return Polynome_Interpolé, Y, X
     
+#Fonction qui gère l'affichage des courbes
 def affichage(points_x, points_y):
-    resultat = li_Lagrange(points_x, points_y)
+    resultat = li_Lagrange(points_x, points_y) 
     plt.plot(resultat[2], resultat[1], label="fonction choisie")
     plt.plot(resultat[2], resultat[0], label="Polynome interpolé")
     plt.title(f"Polynomé interpolé et polynôme choisit, pour n = {n}")
     plt.legend()
     plt.show()
-    
+
 affichage(points_x, points_y)
--- Math_Julie/TP1/test3.py
+++ Math_Julie/TP1/test3.py
@@ -2,18 +2,22 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#Dans ce code, la fonction choisie pour les test est : f(x) = 1/25x^2
+#Définition de l'intervalle d'étude
+
 a, b = -1, 1
 n = 4
+
+#Définition de la fonction "tcheby_points"
 def tcheby_points(a, b, n):
     points_tchebychev = [] #Création du tableau qui contiendra tous les points de tchebychev
     for i in range(n+1):
         x = (a + b)/2 + (b - a)/2 * math.cos((2*i + 1) * math.pi / (2*(n+1))) #Calcul de tous les xn de Tchebychev
         points_tchebychev.append(x)
     return points_tchebychev
-#Dans ce code, la fonction choisie pour les test est : f(x) = 1/25x^2
-#Définition de l'intervalle d'étude
 
-#Définiton du nombre de points de Tchebychev calculé
+
 
 # Définition de trois points dont les coordonnées (x,y) sont respectivement les valeurs de points_x et points_y
 # On choisit pour l'études des points par lesquels passent la fonction choisie. Les abscisses sont respectivement les points de Tchebychev
@@ -21,8 +25,6 @@ points_x = tcheby_points(a, b, n)
 points_y = [1/(1+25*i**2) for i in points_x]
 
 
-#points_x = np.linspace(-1,1,n) 
-    
 def li_Lagrange(points_x, points_y):
     li = []  #Définition d'une liste qui contiendra tous les polynômes "li" de Lagrange
     Polynome_Interpolé = [] #Définition d'une variable qui contiendra le polynôme interpolé
